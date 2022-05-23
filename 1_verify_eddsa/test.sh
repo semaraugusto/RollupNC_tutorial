8744102910c3ba4880501c61c0fe397f007bd7bc
semaraugusto <semaraugusto@dcc.ufmg.br>
2022-05-23 19:02:25 -0300

    removing useless stuff from test.sh file

--- 1_verify_eddsa/test.sh
+++ 1_verify_eddsa/test.sh
@@ -21,14 +21,8 @@ node generate_witness.js eddsa.wasm ../input.json ../witness.wtns
 cd ..
 #
 snarkjs groth16 setup eddsa.r1cs powersOfTau28_hez_final_16.ptau eddsa_0000.zkey
-echo "test" | snarkjs zkey contribute eddsa_0000.zkey eddsa_0001.zkey --name="1st Contributor Name" -v
-snarkjs zkey contribute eddsa_0001.zkey eddsa_0002.zkey --name="Second contribution Name" -v -e="Another random entropy"
-snarkjs zkey export bellman eddsa_0002.zkey  challenge_phase2_0003
-snarkjs zkey bellman contribute bn128 challenge_phase2_0003 response_phase2_0003 -e="some random text"
-snarkjs zkey import bellman eddsa_0002.zkey response_phase2_0003 eddsa_0003.zkey -n="Third contribution name"
-# snarkjs zkey verify eddsa.r1cs powersOfTau28_hez_final_16.ptau eddsa_0003.zkey
-snarkjs zkey beacon eddsa_0003.zkey eddsa_final.zkey 0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f 10 -n="Final Beacon phase2"
-# snarkjs zkey verify eddsa.r1cs powersOfTau28_hez_final_16.ptau eddsa_final.zkey
+echo "test" | snarkjs zkey contribute eddsa_0000.zkey eddsa_final.zkey --name="1st Contributor Name" -v
+snarkjs zkey verify eddsa.r1cs powersOfTau28_hez_final_16.ptau eddsa_final.zkey
 snarkjs zkey export verificationkey eddsa_final.zkey verification_key.json
 snarkjs groth16 prove eddsa_final.zkey witness.wtns proof.json public.json
-# snarkjs groth16 verify verification_key.json public.json proof.json
+snarkjs groth16 verify verification_key.json public.json proof.json
