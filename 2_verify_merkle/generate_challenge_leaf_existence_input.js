3009e4905e888e2530dbd7558cbaa0b723e63e0f
semaraugusto <semaraugusto@dcc.ufmg.br>
2022-05-24 12:13:16 -0300

    cleanup merkle tree. Downgrade to circomlib 0.0.8 to match root dir

--- 2_verify_merkle/generate_challenge_leaf_existence_input.js
+++ 2_verify_merkle/generate_challenge_leaf_existence_input.js
@@ -1,81 +1,51 @@
 const fs = require("fs");
-// const mimcjs = require("../node_modules/circomlibjs/src/mimc7.js");
-// const mimcMerkle = require('./MiMCMerkle.js')
-// const merkle_tree_lib = require("./MerkleTree.js")
-const merkle_tree_lib = require("../merkle-tree-lib")
+const merkle_tree_lib = require("../merkle-tree-lib");
 var ethers = require("ethers");
-// var {poseidon} = require('circomlibjs');
-const buildPoseidonOpt = require("circomlibjs").buildPoseidonOpt;
+const poseidon = require("circomlibjs").poseidon;
 
 var poseidonHash = async function (items) {
-    return buildPoseidonOpt().then((poseidon) => { return ethers.BigNumber.from(poseidon(items)) });
+    return ethers.BigNumber.from(poseidon(items));
 };
 var poseidonHash2 = function (a, b) {
     return poseidonHash([a, b]);
 };
-// const poseidon = require("./poseidon")
-// const { poseidonHash2 } =require('poseidon.ts')
-//
-//
 async function main() {
-    const leaf11 = await poseidonHash2(1,2)
-    const leaf21 = await poseidonHash2(3,4)
-    const leaf31 = await poseidonHash2(5,6)
-    const leaf41 = await poseidonHash2(7,8)
-    const leafArray_1 = [leaf11,leaf21,leaf31,leaf41]
-    console.log(leaf11)
-    console.log(leaf21)
-    console.log(leaf31)
+    const leaf11 = await poseidonHash2(1, 2);
+    const leaf21 = await poseidonHash2(3, 4);
+    const leaf31 = await poseidonHash2(5, 6);
+    const leaf41 = await poseidonHash2(7, 8);
+    const leafArray_1 = [leaf11, leaf21, leaf31, leaf41];
 
-    const tree_1 = new merkle_tree_lib.MerkleTree(2, leafArray_1)
+    const tree_1 = new merkle_tree_lib.MerkleTree(2, leafArray_1);
 
-    const leaf12 = await poseidonHash2(9,0)
-    const leaf22 = await poseidonHash2(1,2)
-    const leaf32 = await poseidonHash2(3,4)
-    const leaf42 = await poseidonHash2(5,6)
-    const leafArray_2 = [leaf12,leaf22,leaf32,leaf42]
+    const leaf12 = await poseidonHash2(9, 0);
+    const leaf22 = await poseidonHash2(1, 2);
+    const leaf32 = await poseidonHash2(3, 4);
+    const leaf42 = await poseidonHash2(5, 6);
+    const leafArray_2 = [leaf12, leaf22, leaf32, leaf42];
 
-    const tree_2 = new merkle_tree_lib.MerkleTree(2, leafArray_2)
+    const tree_2 = new merkle_tree_lib.MerkleTree(2, leafArray_2);
     //
-    // // const tree = mimcMerkle.treeFromLeafArray(leafArray)
     const root_1 = tree_1.root();
     const root_2 = tree_2.root();
-    // console.log(tree)
-    // console.log(leaf1.toString())
-    // console.log(tree._layers[0][0].toString())
-    // console.log(tree._layers[0][1].toString())
-    // console.log(tree._layers[0][1].toString())
-    // console.log(tree._layers[0])
-    const { merkleRoot, pathElements, pathIndices, element } = tree_1.path(0)
-    console.log(merkleRoot)
-    console.log(pathElements)
-    console.log(pathIndices)
-    console.log(element)
-    console.log(leaf11.toString())
-    console.log(leaf11)
-    console.log(leaf11)
-    console.log(leaf11)
-    console.log(leaf11)
-    console.log(leaf11)
-    // let pathElem = pathElements.
-    // const leaf1Pos = [1,1]
-    let pathElem = []
-    let pathIdx = []
-    for(var i =0; i < pathElements.length; i++) {
-        pathElem.push(ethers.BigNumber.from(pathElements[i]).toHexString())
-        pathIdx.push(ethers.BigNumber.from(pathIndices[i]).toHexString())
+
+    const { merkleRoot, pathElements, pathIndices, element } = tree_1.path(2);
+
+    let pathElem = [];
+    let pathIdx = [];
+    for (var i = 0; i < pathElements.length; i++) {
+        pathElem.push(ethers.BigNumber.from(pathElements[i]).toString());
+        pathIdx.push(ethers.BigNumber.from(pathIndices[i]).toString());
     }
     const inputs = {
-        "leaf": [leaf11.toHexString()],
-        "pathElements": pathElem,
-        "pathIndices": pathIdx,
-        "roots": [root_1.toHexString(), root_2.toHexString()],
-    }
+        leaf: [leaf31.toString()],
+        pathElements: pathElem,
+        pathIndices: pathIdx,
+        roots: [root_1.toString(), root_2.toString()],
+    };
 
-    fs.writeFileSync(
-        "./input.json",
-        JSON.stringify(inputs),
-        "utf-8"
-    );
+    fs.writeFileSync("./input.json", JSON.stringify(inputs), "utf-8");
 }
-main().then(() => { console.log("TERMINOU") })
+main().then(() => {
+    console.log("TERMINOU");
+});
--- 2_verify_merkle/test_merkle.sh
+++ 2_verify_merkle/test_merkle.sh
@@ -1,9 +1,11 @@
 alias snarkjs="node /home/semar/Projects/zku/RollupNC_tutorial/3_verify_merkle/node_modules/snarkjs/cli.js" 
-if [ -f ./powersOfTau28_hez_final_16.ptau ]; then
+if [ -f ../powersOfTau28_hez_final_16.ptau ]; then
     echo "powersOfTau28_hez_final_16.ptau already exists. Skipping."
 else
+    pushd ../
     echo 'Downloading powersOfTau28_hez_final_16.ptau'
     wget https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_16.ptau
+    popd
 fi
 
 # echo "verifying ptau"
@@ -20,15 +22,21 @@ cd merkle_js;
 node generate_witness.js merkle.wasm ../input.json ../witness.wtns
 cd ..
 #
-snarkjs groth16 setup merkle.r1cs powersOfTau28_hez_final_16.ptau merkle_0000.zkey
-echo "test" | snarkjs zkey contribute merkle_0000.zkey merkle_0001.zkey --name="1st Contributor Name" -v
-snarkjs zkey contribute merkle_0001.zkey merkle_0002.zkey --name="Second contribution Name" -v -e="Another random entropy"
-snarkjs zkey export bellman merkle_0002.zkey  challenge_phase2_0003
-snarkjs zkey bellman contribute bn128 challenge_phase2_0003 response_phase2_0003 -e="some random text"
-snarkjs zkey import bellman merkle_0002.zkey response_phase2_0003 merkle_0003.zkey -n="Third contribution name"
-snarkjs zkey verify merkle.r1cs powersOfTau28_hez_final_16.ptau merkle_0003.zkey
-snarkjs zkey beacon merkle_0003.zkey merkle_final.zkey 0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f 10 -n="Final Beacon phase2"
-snarkjs zkey verify merkle.r1cs powersOfTau28_hez_final_16.ptau merkle_final.zkey
+snarkjs groth16 setup merkle.r1cs ../powersOfTau28_hez_final_16.ptau merkle_0000.zkey
+echo "test" | snarkjs zkey contribute merkle_0000.zkey merkle_final.zkey --name="1st Contributor Name" -v
+snarkjs zkey verify merkle.r1cs ../powersOfTau28_hez_final_16.ptau merkle_final.zkey
 snarkjs zkey export verificationkey merkle_final.zkey verification_key.json
 snarkjs groth16 prove merkle_final.zkey witness.wtns proof.json public.json
 snarkjs groth16 verify verification_key.json public.json proof.json
+
+# echo "test" | snarkjs zkey contribute merkle_0000.zkey merkle_0001.zkey --name="1st Contributor Name" -v
+# snarkjs zkey contribute merkle_0001.zkey merkle_0002.zkey --name="Second contribution Name" -v -e="Another random entropy"
+# snarkjs zkey export bellman merkle_0002.zkey  challenge_phase2_0003
+# snarkjs zkey bellman contribute bn128 challenge_phase2_0003 response_phase2_0003 -e="some random text"
+# snarkjs zkey import bellman merkle_0002.zkey response_phase2_0003 merkle_0003.zkey -n="Third contribution name"
+# snarkjs zkey verify merkle.r1cs powersOfTau28_hez_final_16.ptau merkle_0003.zkey
+# snarkjs zkey beacon merkle_0003.zkey merkle_final.zkey 0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f 10 -n="Final Beacon phase2"
+# snarkjs zkey verify merkle.r1cs powersOfTau28_hez_final_16.ptau merkle_final.zkey
+# snarkjs zkey export verificationkey merkle_final.zkey verification_key.json
+# snarkjs groth16 prove merkle_final.zkey witness.wtns proof.json public.json
+# snarkjs groth16 verify verification_key.json public.json proof.json
